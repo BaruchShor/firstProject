d2132057c7961a4f9a33b2c997512616c185cd25
Hershi Shor <e0548597918@gmail.com>
2025-05-16 18:41:24 +0300

    finish the project

--- Program.cs
+++ Program.cs
@@ -37,63 +37,69 @@ namespace firstProject
 
         static int userInput()
         {
-            int choose = Convert.ToInt32(Console.ReadLine());
+            int choose;
+            do
+            {
+                printToUser($"please enter a number of the menu.");
+                choose = Convert.ToInt32(Console.ReadLine());
+            } while (choose < 0 || choose > 10);
             return choose;
         }
 
-        static void ElectionManager(int choice, int[] numSeries, string[] ddd)
+        static void ElectionManager(int choice, int[] numSeries)
         {
+            Dictionary<int, Func<int[], int[]>> menuDict = new Dictionary<int, Func<int[], int[]>>()
+            {
+                {3, showSeriesInRevers},
+                {4, showSortSeries},
+                {5, showMaxValue},
+                {6, showMinValue},
+                {7, showSeriesInRevers},
+                {8, showNumValues},
+                {9, showSumSeries},
+            };
             switch (choice)
             {
                 case 1:
-                    changeSeries();
                     break;
                 case 2:
                     showSeries(numSeries);
                     break;
-                case 3:
-                    showSeriesInRevers(ddd);
-                    break;
-                case 4:
-                    showSortSeries();
-                    break;
-                case 5:
-                    showMaxValue();
-                    break;
                 case 7:
-                    showMinValue();
+                    Console.WriteLine(showAverageSeries(numSeries));
                     break;
-                case 8:
-                    showAverageSeries();
-                    break;
-                case 9:
-                    showNumValues();
-                    break;
-                case 10:
-                    showSumSeries();
+                default:
+                    showSeries(menuDict[choice](numSeries));
                     break;
             }
         }
 
-        static void DisplayingTheMenu()
+        static void DisplayingTheMenu(string[] series)
         {
-            string exitDoor;
+            int choose;
             do
             {
-                Menu();
-                exitDoor = Console.ReadLine();
+                choose = SeriesAnalyzer(series);
             }
-            while (exitDoor != "exit");
+            while (choose != 10);
         }
 
         static string[] changeSeries()
         {
+            //function for user to enter his series.
+            printToUser("please enter a corecct series.");
             string[] newSeries = Console.ReadLine().Split(' ');
+            while (newSeries.Length < 3)
+            {
+                printToUser("The series is to short, please enter a corecct series.");
+                newSeries = Console.ReadLine().Split(' ');
+            };
             return newSeries;
         }
 
         static int[] arrayValidation(string[] usersArray)
         {
+            //Validate the type of the series.
             int[] newIntList = new int[usersArray.Length];
             for(int i = 0; i < usersArray.Length; i ++)
             {
@@ -112,67 +118,113 @@ namespace firstProject
             return newIntList;
         }
 
-        static void showSeries(int[] numSeries)
+        static int[] showSeries(int[] numSeries)
         {
             foreach (int value in numSeries)
             {
                 Console.Write($" {value}");
             }
             printToUser("");
+            return numSeries;
         }
 
-        static void showSeriesInRevers(string[] series)
+        static int[] showSeriesInRevers(int[] series)
         {
-            for (int i = series.Length - 1 ; i > 0; i--)
+            //Print the series in revers.
+            int j = 0;
+            int[] reversSeries = new int[series.Length];
+            for (int i = series.Length - 1 ; i >= 0; i--)
             {
-                printToUser(series[i]);
+                reversSeries[j] = series[i];
+                j++;
             }
+            return reversSeries;
         }
 
-        static void showSortSeries()
+        static int[] showSortSeries(int[] series)
         {
-
-        }
-
-        static int showMaxValue(int series)
-        {
+            Array.Sort(series);
             return series;
         }
 
-        static int showMinValue(int series)
+        static int[] showMaxValue(int[] series)
         {
-            return series;
+            int[] max = new int[1];
+            max[0] = series[0];
+            foreach(int num in series)
+            {
+                if (max[0] < num)
+                {
+                    max[0] = num;
+                }
+            }
+            return max;
         }
 
-        static double showAverageSeries(int series)
+        static int[] showMinValue(int[] series)
         {
-            return series;
+            int[] min = new int[1];
+            min[0] = series[0];
+            foreach (int num in series)
+            {
+                if (min[0] > num)
+                {
+                    min[0] = num;
+                }
+            }
+            return min;
         }
 
-        static int showNumValues(int series)
+        static double showAverageSeries(int[] series)
         {
-            return series;
+            int sum = showSumSeries(series)[0];
+            return sum / series.Length;
         }
 
-        static int showSumSeries(int series)
+        static int[] showNumValues(int[] series)
         {
-            return series;
+            int[] numVal = { series.Length };
+            return numVal;
         }
 
-        static bool Exit()
+        static int[] showSumSeries(int[] series)
         {
-            return true;
+            //Sum the values of the series.
+            int[] sum = new int[1];
+            foreach (int num in series)
+            {
+                sum[0] += num;
+            }
+            return sum;
+        }
+
+        static int[] finalSeries(string[] argsSerier, int choose)
+        {
+            if(choose == 1)
+            {
+                int[] newSeries = arrayValidation(changeSeries());
+                return newSeries;
+            }
+            return arrayValidation(argsSerier);
+        }
+
+        static int SeriesAnalyzer(string[] series)
+        {
+            int choose;
+            Menu();
+            choose = userInput();
+            if (choose == 10)
+            {
+                return choose;
+            }
+            int[] seriesForAnilaiz = finalSeries(series, choose);
+            ElectionManager(choose, seriesForAnilaiz);
+            return choose;
         }
 
         static void Main(string[] args)
         {
-            //printToUser("kkkk");
-            //printToUser(123);
-            //string[] aaa = {"123","489","8s9", "123456" };
-            //showSeries(arrayValidation(aaa));
-            //Console.WriteLine();
-            showSeriesInRevers(args);
-
+            DisplayingTheMenu(args);
         }
     }
 }
